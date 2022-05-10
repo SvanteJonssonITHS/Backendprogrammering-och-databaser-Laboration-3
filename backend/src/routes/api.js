9096c24640cabbcac8e452e285855aa5f704d657
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 09:59:10 +0200

    GET route for mongoose collection dices

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,5 +1,8 @@
-// Dependencies
+// External Dependencies
 const express = require('express');
+
+// Internal Dependencies
+const DiceModel = require('../models/Dice');
 
 // Variable declaration
 const router = express.Router();
@@ -9,6 +12,27 @@ router.get('/', async (_req, res) => {
 		success: true,
 		message: 'Welcome to the API'
 	});
+});
+
+router.get('/diceRolls', async (req, res) => {
+	const limit = req.query.limit ? req.query.limit : 0;
+
+	try {
+		let allDiceRolls = await DiceModel.find().sort({ value: -1 }).limit(limit);
+		res.status(200).json({
+			success: true,
+			message: 'Dice rolls fetched successfully',
+			error: '',
+			data: allDiceRolls
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: 'Failed to fetch dice rolls',
+			error: error.message || 'Something went wrong retrieving all dice rolls',
+			data: []
+		});
+	}
 });
 
 module.exports = router;
