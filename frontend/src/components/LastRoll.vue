6b39ffd206ea74ea6bb05a3aa36181c0d993dd6f
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 18:39:31 +0200

    Created `LastRoll`

---
+++ frontend/src/components/LastRoll.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="flex flex-col bg-stone-300 p-2 rounded-md">
+		<h2 class="font-semibold text-lg mr-auto">Last roll</h2>
+		<p v-if="value >= 6 && value <= 36 && user != ''">
+			<span class="font-semibold">{{ user }}</span> rolled a
+			{{ value }}
+		</p>
+		<p v-else>No one has rolled yet</p>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'LastRoll',
+		props: {
+			user: {
+				type: String,
+				default: '',
+				Required: true
+			},
+			value: {
+				type: Number,
+				default: 1,
+				Required: true,
+				validator(value) {
+					return value >= 6 && value <= 36;
+				}
+			}
+		}
+	};
+</script>
