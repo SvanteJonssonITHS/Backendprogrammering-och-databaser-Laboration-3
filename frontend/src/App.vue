8d89cb095abc2fa7ebf29036d44692e2b00eb2a3
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 14:35:58 +0200

    Created `UserModal`

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,35 @@
-<template><h1 class="text-3xl font-bold">Hello world!</h1></template>
+<template>
+	<UserModal v-if="showModal" @close="showModal = false" />
+</template>
 
 <script>
+	import UserModal from './components/UserModal.vue';
+
 	export default {
-		name: 'App'
+		name: 'App',
+		components: {
+			UserModal
+		},
+		data() {
+			return {
+				username: '',
+				showModal: true
+			};
+		},
+		mounted() {
+			this.checkUsername();
+		},
+		methods: {
+			checkUsername() {
+				// check if the username is set in the sessionStorage
+				const username = sessionStorage.getItem('username');
+
+				// if not, show the modal
+				if (username) {
+					this.showModal = false;
+					this.username = username;
+				}
+			}
+		}
 	};
 </script>
---
+++ frontend/src/components/UserModal.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="absolute top-0 left-0 w-screen min-h-screen bg-black/50 flex">
+		<form @submit.prevent="submit" class="m-auto bg-white bg-black p-10 rounded-md">
+			<h2 class="text-3xl font-semibold">Set site username</h2>
+			<label class="flex flex-col my-10">
+				<p class="text-lg mb-2">Username:</p>
+				<input type="text" v-model="username" class="border-2 border-neutral-500 p-2 outline-none rounded-md" />
+			</label>
+			<button
+				@click="submit"
+				class="bg-blue-500 hover:bg-blue-400 transition ease duration-150 text-white px-4 py-2 rounded-md float-right"
+			>
+				Set username
+			</button>
+		</form>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'UserModal',
+		emits: ['close'],
+		data() {
+			return {
+				username: ''
+			};
+		},
+		methods: {
+			submit() {
+				if (this.username != '' && this.username.length > 0) {
+					sessionStorage.setItem('username', this.username);
+					this.$emit('close');
+				}
+			}
+		}
+	};
+</script>
