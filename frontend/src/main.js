bd210af7e3fce755505c8f525de61c0bdfa278de
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 11:30:04 +0200

    Installed `VueSocketIO`

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 // External Dependencies import
 import { createApp } from 'vue';
+import { io } from 'socket.io-client';
+import VueSocketIO from 'vue-3-socket.io';
 
 // Internal Dependencies import
 import App from './App.vue';
@@ -7,6 +9,10 @@ import './styles/tailwind.css';
 
 //Creats the Vue app
 const app = createApp(App);
+
+// Connect to socket.io
+const socketio = new VueSocketIO({ connection: io(window.location.origin) });
+app.use(socketio);
 
 //Mounts app to div with id app
 app.mount('#app');
