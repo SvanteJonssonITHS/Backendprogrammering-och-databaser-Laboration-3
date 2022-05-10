aa980d87dde97e10a63adb50eb89fc4435fb7531
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 16:03:33 +0200

    Created `Dice`

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,32 @@
 <template>
 	<UserModal v-if="showModal" @close="showModal = false" />
+	<main class="w-screen h-m-screen flex flex-col justify-center items-center">
+		<h2 class="text-7xl font-semibold">{{ totalValue }}</h2>
+		<ul class="w-11/12 md:3/5 h-full flex justify-center flex-wrap">
+			<li v-for="(dice, index) in diceArray" :key="index">
+				<Dice :value="dice" class="m-2" />
+			</li>
+		</ul>
+		<button class="bg-blue-500 text-white font-semibold px-4 py-2 rounded-md" @click="rollDice">Roll dice</button>
+	</main>
 </template>
 
 <script>
 	import UserModal from './components/UserModal.vue';
+	import Dice from './components/Dice.vue';
 
 	export default {
 		name: 'App',
 		components: {
-			UserModal
+			UserModal,
+			Dice
 		},
 		data() {
 			return {
 				username: '',
-				showModal: true
+				showModal: true,
+				diceArray: [0, 0, 0, 0, 0, 0],
+				totalValue: 0
 			};
 		},
 		mounted() {
@@ -28,6 +41,16 @@
 				if (username) {
 					this.showModal = false;
 					this.username = username;
+				}
+			},
+			async rollDice() {
+				const request = await fetch('/api/rollDice?amount=6');
+				const response = await request.json();
+
+				// if the request was successful, set the dice array
+				if (response.data[0]) {
+					this.totalValue = response.data[0].total;
+					this.diceArray = response.data[0].dice;
 				}
 			}
 		}
---
+++ frontend/src/components/Dice.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="dice" v-if="value >= 1 && value <= 6">
+		<span
+			:class="`dot 
+            ${value == 1 && index == 1 ? 'mm' : ''}
+            ${value == 2 && index == 1 ? 'tl' : ''}
+            ${value == 2 && index == 2 ? 'br' : ''}
+            ${value == 3 && index == 1 ? 'tl' : ''}
+            ${value == 3 && index == 2 ? 'mm' : ''}
+            ${value == 3 && index == 3 ? 'br' : ''}
+            ${value == 4 && index == 1 ? 'tl' : ''}
+            ${value == 4 && index == 2 ? 'tr' : ''}
+            ${value == 4 && index == 3 ? 'bl' : ''}
+            ${value == 4 && index == 4 ? 'br' : ''}
+            ${value == 5 && index == 1 ? 'tl' : ''}
+            ${value == 5 && index == 2 ? 'tr' : ''}
+            ${value == 5 && index == 3 ? 'mm' : ''}
+            ${value == 5 && index == 4 ? 'bl' : ''}
+            ${value == 5 && index == 5 ? 'br' : ''}
+            ${value == 6 && index == 1 ? 'tl' : ''}
+            ${value == 6 && index == 2 ? 'tr' : ''}
+            ${value == 6 && index == 3 ? 'ml' : ''}
+            ${value == 6 && index == 4 ? 'mr' : ''}
+            ${value == 6 && index == 5 ? 'bl' : ''}
+            ${value == 6 && index == 6 ? 'br' : ''}
+            `"
+			v-for="index in value"
+			:key="index"
+		/>
+	</div>
+	<div class="dice" v-else>
+		<p class="mm text-7xl font-bold text-white">?</p>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'Dice',
+		props: {
+			value: {
+				type: Number,
+				default: 1,
+				validator(value) {
+					return value >= 1 && value <= 6;
+				}
+			}
+		}
+	};
+</script>
+
+<style>
+	.dice {
+		@apply bg-red-500 w-32 h-32 rounded-xl grid grid-cols-3 grid-rows-3 place-items-center;
+	}
+	.dot {
+		@apply bg-white w-7 h-7 rounded-full block;
+	}
+	.tl {
+		@apply row-start-1 col-start-1;
+	}
+	.tr {
+		@apply row-start-1 col-start-3;
+	}
+	.ml {
+		@apply row-start-2 col-start-1;
+	}
+	.mm {
+		@apply row-start-2 col-start-2;
+	}
+	.mr {
+		@apply row-start-2 col-start-3;
+	}
+	.bl {
+		@apply row-start-3 col-start-1;
+	}
+	.br {
+		@apply row-start-3 col-start-3;
+	}
+</style>
