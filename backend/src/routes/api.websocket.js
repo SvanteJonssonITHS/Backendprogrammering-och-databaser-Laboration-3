fc69395937892c0dc1a405eff2cfcc334db40180
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 11:10:12 +0200

    Updated socket function `message`

--- backend/src/routes/api.websocket.js
+++ backend/src/routes/api.websocket.js
@@ -1,13 +1,26 @@
 let socketIO;
 
 const DiceModel = require('../models/Dice');
+const MessageModel = require('../models/Message');
 
 exports.websocketRoutes = (io) => {
 	socketIO = io;
 
 	socketIO.on('connection', (socket) => {
-		socket.on('message', async (data) => {
-			io.emit('message', data);
+		socket.on('message', async (user, value) => {
+			if (user && value) {
+				// Create a new message object
+				const message = new MessageModel({
+					user: user,
+					value: value
+				});
+
+				// Save the message to the database
+				await message.save();
+
+				// Emit the message to all connected clients
+				socketIO.emit('message', message);
+			}
 		});
 		socket.on('diceRoll', async (user, value) => {
 			if (user && value) {
@@ -22,7 +35,7 @@ exports.websocketRoutes = (io) => {
 				diceRoll.save();
 
 				// Emit dice roll to all clients
-				socketIO.emit('diceRoll', { user, value });
+				socketIO.emit('diceRoll', diceRoll);
 			}
 		});
 	});
