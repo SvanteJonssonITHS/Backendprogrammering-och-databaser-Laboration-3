f1c54012e8a9af89475f45db07ad79e8a947ff8a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-10 18:39:02 +0200

    Created `TopList`

---
+++ frontend/src/components/TopList.vue
@@ -0,0 +1,19 @@
+<template>
+	<ul v-if="rolls.length > 0" class="flex flex-col bg-stone-300 p-2 list-decimal list-inside rounded-md">
+		<li v-for="(item, index) in rolls" :key="index" class="border-b-2 border-stone-400 p-2">
+			<span class="float-right">{{ item.user }} : {{ item.value }}</span>
+		</li>
+	</ul>
+</template>
+<script>
+	export default {
+		name: 'LastRoll',
+		props: {
+			rolls: {
+				type: Array,
+				Required: true,
+				default: () => []
+			}
+		}
+	};
+</script>
